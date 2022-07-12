4f36fd935fd7fe5959549ab290b99ea8b7967b93
Lyubomir Nachev <48567009+LyubomirNachev@users.noreply.github.com>
2022-07-12 15:47:04 +0300

    fully software controlls through serial input

--- digital_pot.ino
+++ digital_pot.ino
@@ -1,57 +1,79 @@
-const int up = 8;  // izpolzvame const int za read only variables
-const int down =  7;  
-int up_state = 0;
-int down_state = 0;
-
-const int inc =  10;    
-const int ud =  9;   
-
-int loop_cnt = 0;
+const float V_REF = 5.0;     
+const int UP = 0;
+const int DOWN = 1;
+const int UD_PIN = 8;       
+const int INC_PIN = 9;    
+const int CS_PIN = 10;      
+const int WIPER_PIN = A5;     
+float voltage = 0;
 
 void setup() {
+  pinMode (CS_PIN, OUTPUT);
+  pinMode (UD_PIN, OUTPUT);
+  pinMode (INC_PIN, OUTPUT);
   Serial.begin(9600);
-  
-  pinMode(up, INPUT_PULLUP); 
-  pinMode(down, INPUT_PULLUP);
-  pinMode(inc, OUTPUT);
-  pinMode(ud, OUTPUT);
-  digitalWrite(inc, HIGH);
-  digitalWrite(ud, HIGH);
-  
-  while (!Serial) {
-    delay(1);
-  }
 
+  digitalWrite(INC_PIN, HIGH);
+  digitalWrite(CS_PIN, LOW);                  // Enable the X9C103P chip
+  //Serial.print ("Initial Voltage Setting: ");
+  PrintVoltage();                             // Print X9C103P power up value
 }
 
-const int loopPeriod = 50;  //Loop period equals three times of this value, ex) 50 * 3 = 150ms
-
 void loop() {
+  if (Serial.available()) DoSerial();  // loop looking for user input
+  Serial.println(analogRead(A4));
+}
 
-  loop_cnt = loop_cnt + 1;
-  
-    //Read the level of Up/Down button
-  up_state = digitalRead(up);
-  down_state = digitalRead(down);
-  
-    //Check if Up/Down Button is Pressed
-    //When Button is Pressed, Level becomes LOW
-  if (up_state == LOW){
-    digitalWrite(ud, HIGH);
-    delay(loopPeriod);
-    digitalWrite(inc, LOW);
-    delay(loopPeriod);
-    digitalWrite(inc, HIGH);
-    delay(loopPeriod);
-  }else if (down_state == LOW){
-    digitalWrite(ud, LOW);
-    delay(loopPeriod);
-    digitalWrite(inc, LOW);
-    delay(loopPeriod);
-    digitalWrite(inc, HIGH);
-    delay(loopPeriod);
-  }else{
-      delay(loopPeriod*3);
-   }
+void DoSerial()
+{
+  char ch = toupper(Serial.read());   // Read the character we received
+  // and convert to upper case
+  switch (ch) {
+    case 'S':                         // Save settings
+      digitalWrite(INC_PIN, HIGH);
+      digitalWrite(CS_PIN, HIGH);
+      delay(100);
+      digitalWrite(CS_PIN, LOW);
+      //Serial.println("Setting Saved");
+      break;
+    case 'U':                         // Increment setting
+      Move_Wiper(UP);
+      //Serial.print("Incrementing Value");
+      PrintVoltage();
+      break;
+    case 'D':                         // Decrement setting
+      Move_Wiper(DOWN);
+      //Serial.print("Decrementing Value");
+      PrintVoltage();
+      break;
+    default:
+      break;
+  }
+}
+void PrintVoltage()
+{
+  int sampleADC = analogRead(WIPER_PIN);      // Take reading on wiper pin
+  float volts = (sampleADC * V_REF) / 1023.0; // Convert to voltage
+  //Serial.print("   ADC = ");
+  //Serial.print(sampleADC);
+  //Serial.print("  Voltage = ");
+  //Serial.println(volts, 3);
+}
 
+void Move_Wiper(int direction)
+{
+  switch (direction) {
+    case UP:
+      digitalWrite(UD_PIN, HIGH); delayMicroseconds(5); 
+      digitalWrite(INC_PIN, LOW); delayMicroseconds(5); 
+      digitalWrite(INC_PIN, HIGH);
+      break;
+    case DOWN:
+      digitalWrite(UD_PIN, LOW); delayMicroseconds(5); 
+      digitalWrite(INC_PIN, LOW); delayMicroseconds(5); 
+      digitalWrite(INC_PIN, HIGH);
+      break;
+    default:
+      break;
+  }
 }
